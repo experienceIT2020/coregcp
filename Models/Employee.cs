5639cc2f610d99c64876ba6f3ac66b37d7e44e7a
Ravishankar Muthuswamy <rmuthuswamy@onemagnify.com>
2023-03-22 13:02:53 -0400

    Strapi Update

--- Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-namespace CoreGCP.Models
-{
-    public class Employee
-    {
-        public int? Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-    }
-}
--- Utils/ConfigOptions/GCSConfigOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace CoreGCP.Utils.ConfigOptions
-{
-    public class GCSConfigOptions
-    {
-        public string? GCPStorageAuthFile { get; set; }
-        public string? GoogleCloudStorageBucketName { get; set; }
-
-    }
-}
