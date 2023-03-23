0d45510cef10dfe177fe3102da443803b318f55b
Ravishankar Muthuswamy <rmuthuswamy@onemagnify.com>
2023-03-22 21:56:47 -0400

    config update

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         connection: {
             host: env('DB_HOST', 'localhost'),
-            port: env('DDB_PORT', 5432),
+            port: env('DB_PORT', 5432),
             database: env('DB_NAME'),
             user: env('DB_USER'),
             password: env('DB_PASS'),
